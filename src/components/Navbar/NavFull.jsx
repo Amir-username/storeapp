6135fd28501d4d4e9416e399743e56ab09345ecc
Amir <najiamirdesktop@gmail.com>
2023-09-18 13:02:06 +0330

    added mobile view navbar

--- src/components/Navbar/NavFull.jsx
+++ src/components/Navbar/NavFull.jsx
@@ -1,11 +1,21 @@
 import NavBrand from "./NavBrand";
 import NavItem from "./NavItem";
+import NAVMENUN from "../../assets/NAVMENU.svg";
 
-function NavFull({isOpen}) {
+function NavFull({ isOpen }) {
   return (
-    <div className="flex p-3 h-16 w-screen justify-center md:justify-start">
-        <NavBrand />
-    </div>
+    <>
+      <div className="flex p-3 gap-2 h-16 w-screen justify-center md:justify-start">
+        <div className="flex w-full items-center justify-center mt-1">
+          <NavBrand />
+        </div>
+        <img
+          src={NAVMENUN}
+          alt="menu"
+          className="w-12 h-12 cursor-pointer active:bg-gray-100 rounded-full"
+        />
+      </div>
+    </>
   );
 }
 
