ac4bef2198b35834e2b1df70f6e7c8a9e638de9f
Amir <najiamirdesktop@gmail.com>
2023-09-18 15:32:25 +0330

    update navbar

--- src/components/Navbar/NavFull.jsx
+++ src/components/Navbar/NavFull.jsx
@@ -2,21 +2,23 @@ import NavBrand from "./NavBrand";
 import NavItem from "./NavItem";
 import NAVMENUN from "../../assets/NAVMENU.svg";
 import NavItemFull from "./NavItemFull";
+import SearchForm from "./SearchForm";
 
 function NavFull({ setIsOpen }) {
   return (
     <>
-      <div className="flex p-3 gap-2 h-16 w-screen justify-center md:justify-start">
+      <div className="flex p-3 gap-64 h-16 w-screen justify-center md:justify-start">
         <div className="flex w-full md:w-32 items-center justify-center mt-1">
           <NavBrand />
         </div>
-        <div className="hidden md:visible md:flex py-2 gap-10 w-full pl-36">
+        <div className="hidden md:visible md:flex py-2 gap-10 w-fit mt-0.5">
           <NavItemFull name={"home"}/>
           <NavItemFull name={"home"}/>
           <NavItemFull name={"home"}/>
           <NavItemFull name={"home"}/>
           <NavItemFull name={"home"}/>
         </div>
+        <SearchForm />
         <img
           onClick={() => setIsOpen(true)}
           src={NAVMENUN}
