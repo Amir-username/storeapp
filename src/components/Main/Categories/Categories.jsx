9b1e7bd2f1ddfcd2af87ea586318d944881e4b4c
Amir <najiamirdesktop@gmail.com>
2023-09-20 15:28:48 +0330

    added categories mobile view

--- src/components/Main/Categories/Categories.jsx
+++ src/components/Main/Categories/Categories.jsx
@@ -9,20 +9,20 @@ import DOUBLEUNDERLINE from "../../../assets/DOUBLEUNDERLINE.svg";
 
 function Categories() {
   return (
-    <div>
+    <>
       <div className="flex flex-col gap-0 mt-40 mb-10 justify-center items-center">
-        <p className="text-gray-600 text-center text-4xl font-bold">
+        <p className="text-gray-600 text-center md:text-4xl text-2xl font-bold">
           categories
         </p>
-        <img src={DOUBLEUNDERLINE} alt="underline" className="w-12 h-12" />
+        <img src={DOUBLEUNDERLINE} alt="underline" className="md:w-12 md:h-12 w-8 h-8"/>
       </div>
-      <div className="flex flex-wrap gap-10 justify-center items-center  rounded-lg p-5 mx-80 pt-8">
+      <div className="flex flex-wrap md:gap-10 gap-5 justify-center items-center rounded-lg p-5 md:mx-80 md:pt-8 pt-4">
         <CategoryItem image={ELECTRONICCAT} name={"electronic"} />
         <CategoryItem image={JEWELERY} name={"jewelery"} />
         <CategoryItem image={MENCAT} name={"men's clothing"} />
         <CategoryItem image={WOMENCAT} name={"women's clothing"} />
       </div>
-    </div>
+    </>
   );
 }
 
