96430874ea0ca8336d4054501e8840696e9591f6
Amir <najiamirdesktop@gmail.com>
2023-10-14 13:00:53 +0330

    update category section

--- src/components/Main/Categories/CategoryItem.jsx
+++ src/components/Main/Categories/CategoryItem.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 function CategoryItem({ name, image }) {
   return (
-    <div
-      className="flex justify-center items-center ring-1 ring-gray-300 duration-500 hover:shadow-2xl cursor-pointer
+    <Link to={`products/${name}`}>
+      <div
+        className="flex justify-center items-center ring-1 ring-gray-300 duration-500 hover:shadow-2xl cursor-pointer
          rounded-lg w-96 h-96"
-    >
-      <div className="flex flex-col justify-center items-center gap-y-3">
-        <img src={image} alt="category" className="w-32 h-32" />
-        <p className="text-center text-gray-700 text-xl capitalize">{name}</p>
+      >
+        <div className="flex flex-col justify-center items-center gap-y-3">
+          <img src={image} alt="category" className="w-32 h-32" />
+          <p className="text-center text-gray-700 text-xl capitalize">{name}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
